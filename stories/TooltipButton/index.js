1329aa24eea28d763c555aec9a6e97b4fe30cd64
aaxuyun <784266498@qq.com>
2019-10-09 18:09:40 +0800

    update

--- stories/TooltipButton/index.js
+++ stories/TooltipButton/index.js
@@ -3,6 +3,9 @@ import CodeExample from 'wix-storybook-utils/CodeExample'
 import TooltipButtonBasicExample from './ExampleBasic'
 import ExampleBasicRaw from  '!raw-loader!./ExampleBasic'
 import readme from './README.md'
+import css from "@emotion/css"
+
+import { Row, Col, Card } from 'antd'
 
 const exampleContainerStyle = {
     display: 'flex',
@@ -12,14 +15,29 @@ const exampleContainerStyle = {
     justifyContent: 'center',
   }
 
+const renderComponents = () =>{
+  return(
+    <TooltipButtonBasicExample/>
+  )
+}
 const component = () => (
-    <div style={exampleContainerStyle}>
-            <TooltipButtonBasicExample/>
-            <CodeExample title="Standard with icons" code={ExampleBasicRaw}>
-                
-            </CodeExample>
-     </div>
-    
+    <Row gutter={16}>
+      <Col span={12} >
+        <Card title={renderComponents() } className={css`
+        .markdown-body pre{
+          text-align:left;
+        }
+        `}
+        actions={[<CodeExample  code={ExampleBasicRaw} />]}>
+          Standard with icons
+        </Card>
+      </Col>
+      <Col span={12}>
+        <Card title={renderComponents()} actions={[<CodeExample code={ExampleBasicRaw}/>]}>
+            Standard with icons
+          </Card>
+      </Col>
+    </Row>
 )
 
 export default [readme, component]
